1b658b99a9dc4099863e35f626ca08279f88ba7d
Anshul Bajpai <bajpai.anshul@gmail.com>
2013-10-28 23:56:56 +0530

    Fixed #10

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,6 +1,16 @@
 define(['modules/app','service/loginService','service/contactsService','service/projectsService'] , function (app) {
   app.controller('loginController',['$scope', '$cookieStore', '$location', '$sessionStorage','loginService', 'contactsService', 'projectsService',function($scope, $cookieStore, $location, $sessionStorage, loginService, contactsService, projectsService){
+    var apiKey = $cookieStore.get('apiKey');
+    var accountId = $cookieStore.get('accountId');
+    var contacts = $sessionStorage.contacts;
+    var projects = $sessionStorage.projects;
+
+    if(apiKey && accountId && contacts && projects){
+      $location.path('/entries/current'); 
+    }
+
     $scope.login = function(){
+
       loginService.getCurrentAccount($scope.apiKey, {
         success : function(account) {
           $cookieStore.put('apiKey',$scope.apiKey);
