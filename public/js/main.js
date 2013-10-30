20218bd840528803b1a69ade11998ab620a52d33
Anshul Bajpai <bajpai.anshul@gmail.com>
2013-10-30 15:29:59 +0530

    Added better UI using bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,8 @@ require.config({
 	paths : {
 		'angular' : '//ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular.min',
 		'angular-cookies' : '//ajax.googleapis.com/ajax/libs/angularjs/1.0.8/angular-cookies.min',
-		'angular-storage' : '//rawgithub.com/gsklee/ngStorage/master/ngStorage.min'
+		'angular-storage' : '//rawgithub.com/gsklee/ngStorage/master/ngStorage.min',
+		'angular-bootstrap' : '//raw.github.com/angular-ui/bootstrap/gh-pages/ui-bootstrap-tpls-0.6.0'
 	},
 	shim : {
 		'angular-cookies' : {
@@ -14,12 +15,16 @@ require.config({
 			'exports' : 'angular-storage',
 			'deps' : ['angular']	
 		},
+		'angular-bootstrap' : {
+			'exports' : 'angular-bootstrap',
+			'deps' : ['angular']	
+		},
 		'angular' : {
 			'exports' : 'angular'
 		}
 	}
 });
 
-require(['angular','modules/app','directives/authDirective','routes/loginRoutes','routes/entriesPageRoutes',''], function(angular){
+require(['angular','directives/authDirective','routes/loginRoutes','routes/entriesPageRoutes'], function(angular){
 	angular.bootstrap(document, ['app']);
 });
