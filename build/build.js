2dcb10bc6f0f4599cb841fd4abf089ee079436ce
Anshul Bajpai <bajpai.anshul@gmail.com>
2013-11-05 03:13:00 +0530

    Added minification support

---
+++ build/build.js
@@ -0,0 +1,13 @@
+({	
+	baseUrl: "../public/js/",
+ 	mainConfigFile: '../public/js/main.js',
+    paths: {
+        'angular': "empty:",
+        'angular-storage': "empty:",
+        'angular-cookies': "empty:",
+        'jquery': "empty:",
+        'bootstrap': "empty:"
+    },
+    name: "main",
+    out : "../public/js/main.min.js"
+})
