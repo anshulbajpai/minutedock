09839b734c2fad5d499958edc036fb5866cde7e4
Anshul Bajpai <bajpai.anshul@gmail.com>
2013-11-07 15:35:40 +0530

    Refactored pagination controller

--- public/js/controllers/paginationController.js
+++ public/js/controllers/paginationController.js
@@ -2,8 +2,7 @@ define(['modules/app','models/paginationModel'] , function (app, PaginationModel
 
   app.controller('paginationController',['$scope','$routeParams', function($scope, $routeParams){  	
 
-    var model = new PaginationModel($routeParams.month, $routeParams.year);
-    $scope.model = model;
+    $scope.model = new PaginationModel($routeParams.month, $routeParams.year);
 
   }]);
 
