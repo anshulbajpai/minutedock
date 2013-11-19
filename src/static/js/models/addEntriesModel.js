9a7323c92556c75af0f8248123036cb8bf2f6939
Abhimanyu Chakravarty <viperzenator@gmail.com>
2013-11-19 11:42:11 +0530

    Modified calendar to be more modular and concise

--- src/static/js/models/addEntriesModel.js
+++ src/static/js/models/addEntriesModel.js
@@ -1,13 +1,13 @@
 define([] , function () {
 
   var getDates = function(month, year) {
-      var dates = [];
-      var firstDate = new Date(year, month-1, 1);
-      var lastDate = new Date(year, month, 0);
-      for(var date = firstDate; date <= lastDate; date = new Date(year,month-1, date.getDate() + 1)){
-        dates.push(date);
-      }
-      return dates;
+    var dates = [];
+    var firstDate = new Date(year, month-1, 1);
+    var lastDate = new Date(year, month, 0);
+    for(var date = firstDate; date <= lastDate; date = new Date(year,month-1, date.getDate() + 1)){
+      dates.push(date);
+    }
+    return dates;
   };
 
   var getWeekdays = function(dates) {
@@ -16,76 +16,78 @@ define([] , function () {
   };  
 
   var fillDates = function(allDates) {
-  	  var dates = [];
-      var firstDate = allDates[0];
-      var startingDay = firstDate.getDay();
-      var monthLength = allDates.length;
+    var dates = [];
+    var firstDate = allDates[0];
+    var startingDay = firstDate.getDay();
+    var monthLength = allDates.length;
+    var days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 
-      var day = 1;
-      for (var i = 0; i < 6; i++) {
-        if(day > monthLength){
-          break;
-        }
-        dates.push([]);
-        for (var j = 0; j <= 6; j++) {
-          if(i == 0 && j < startingDay){
-            dates[i].push(null);
-          }
-          else if (day <= monthLength){
-            dates[i].push(allDates[day-1]);
-            day++;          
-          }
-          else{
-           dates[i].push(null); 
-          }
-        }
+    var day = 1;
+    for (var i = 0; i < 6; i++) {
+      if(day > monthLength){
+        break;
       }
-      return dates;
-   };
+      dates.push([]);
+      for (var j = 0; j <= 6; j++) {
+        if(i == 0 && j < startingDay){
+          dates[i].push(null);
+        }
+        else if (day <= monthLength){
+          dates[i].push({date:allDates[day-1], day:days[j]});            
+          day++;          
+        }
+        else{
+         dates[i].push(null); 
+       }
+     }
+   }
+   console.log(dates);
+   return dates;
+ };
 
-  var AddEntriesModel = function(contacts, projects, currentMonth, currentYear) {
-    var allDates = getDates(currentMonth, currentYear);
-    this._weekdays = getWeekdays(allDates);
-    
-    this.contacts = contacts;
-    this.projects = projects;
+ var AddEntriesModel = function(contacts, projects, currentMonth, currentYear) {
+  var allDates = getDates(currentMonth, currentYear);
+  this._weekdays = getWeekdays(allDates);
 
-    this.dates = fillDates(allDates);
-    this.allWeekdaysSelected = false;
+  this.contacts = contacts;
+  this.projects = projects;
+
+  this.dates = fillDates(allDates);
+  this.allWeekdaysSelected = false;
+  this.selectedDates = [];
+};
+
+AddEntriesModel.prototype.isDateInFuture = function(date) {
+  return date > new Date();
+};
+
+AddEntriesModel.prototype.isDateSelected = function(date) {
+  return this.selectedDates.indexOf(date) > -1;
+};
+
+AddEntriesModel.prototype.isNoDateSelected = function() {
+  return this.selectedDates.length == 0;
+};  
+
+AddEntriesModel.prototype.toggleAllWeekdays = function() {
+  if(this.allWeekdaysSelected) {
+    this.selectedDates = this._weekdays;        
+  }
+  else {
     this.selectedDates = [];
-  };
+  }
+};
 
-  AddEntriesModel.prototype.isDateInFuture = function(date) {
-    return date > new Date();
-  };
+AddEntriesModel.prototype.toggleDate = function(date) {
+  this.allWeekdaysSelected = false;
+  var id = this.selectedDates.indexOf(date);
+  if(id > -1){
+    this.selectedDates.splice(id, 1);
+  }else{
+    this.selectedDates.push(date);
+  }
+};
 
-  AddEntriesModel.prototype.isDateSelected = function(date) {
-    return this.selectedDates.indexOf(date) > -1;
-  };
-
-  AddEntriesModel.prototype.isNoDateSelected = function() {
-    return this.selectedDates.length == 0;
-  };  
-
-  AddEntriesModel.prototype.toggleAllWeekdays = function() {
-      if(this.allWeekdaysSelected) {
-        this.selectedDates = this._weekdays;        
-      }
-      else {
-        this.selectedDates = [];
-      }
-  };
-
-  AddEntriesModel.prototype.toggleDate = function(date) {
-      this.allWeekdaysSelected = false;
-      var id = this.selectedDates.indexOf(date);
-      if(id > -1){
-        this.selectedDates.splice(id, 1);
-      }else{
-        this.selectedDates.push(date);
-      }
-  };
-
-  return AddEntriesModel;
+return AddEntriesModel;
 
 });	
