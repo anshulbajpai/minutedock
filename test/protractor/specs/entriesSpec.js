caac0e4cf65534972498e549e8b9bcd8534552ab
Anshul Bajpai <bajpai.anshul@gmail.com>
2014-06-18 13:32:31 +0530

    Added test to assert entry data

--- test/protractor/specs/entriesSpec.js
+++ test/protractor/specs/entriesSpec.js
@@ -4,9 +4,23 @@ describe('app', function() {
 		persistUser();		
 	});
 	
-	it('should show all entries', function() {
+	it('should show all entries for current month', function() {
 		driver.get("/",{id:'viewEntries'});
-		expect($$('.entry').count()).toBe(3);
+		var entries = $$('.entry');
+		expect(entries.count()).toBe(3);
+		var entryData = entries.map(function(entry) {
+			return { 
+				date : entry.$(".date").getText(),
+				contact : entry.$(".contact").getText(),
+				project : entry.$(".project").getText(),
+				duration : entry.$(".duration").getText()
+			};
+		});
+		expect(entryData).toEqual([
+			{date:"18/6/2014",contact:"contact1",project:"project1",duration : "8"},
+			{date:"18/6/2014",contact:"contact1",project:"project1",duration : "8"},
+			{date:"18/6/2014",contact:"contact2",project:"project2",duration : "8"},
+		]);
 	});
 	
 });
