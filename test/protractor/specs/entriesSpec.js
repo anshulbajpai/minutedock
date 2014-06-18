b03f9172ad1c185fa3b59d76319efc0fd409f6e3
Anshul Bajpai <bajpai.anshul@gmail.com>
2014-06-18 15:37:36 +0530

    Added test for previous and next

--- test/protractor/specs/entriesSpec.js
+++ test/protractor/specs/entriesSpec.js
@@ -9,7 +9,55 @@ describe('app', function() {
 		var entries = $$('.entry');
 		expect(entries.count()).toBe(3);
 		
-		var entryData = entries.map(function(entry) {
+		var entryData = createEntriesData(entries);
+		
+		var date = new Date();
+		var firstDay = formatDate(new Date(date.getFullYear(), date.getMonth(), 1));
+		var lastDay = formatDate(new Date(date.getFullYear(), date.getMonth() + 1, 0));
+		
+		assertEntriesData(firstDay,lastDay, entryData);
+	});
+
+	it('previous and next month should show corresponding month entries', function() {
+		driver.get("/",{id:'viewEntries'});
+
+	    $('.previous a').click(); 		
+
+		var entries = $$('.entry');
+		expect(entries.count()).toBe(3);
+		
+		var entryData = createEntriesData(entries);
+		
+		var date = new Date();
+		var firstDay = formatDate(new Date(date.getFullYear(), date.getMonth() -1 , 1));
+		var lastDay = formatDate(new Date(date.getFullYear(), date.getMonth(), 0));
+		
+		assertEntriesData(firstDay,lastDay, entryData);
+		
+		$('.next a').click(); 	
+
+		entries = $$('.entry');
+		expect(entries.count()).toBe(3);
+		
+		entryData = createEntriesData(entries);
+		
+		firstDay = formatDate(new Date(date.getFullYear(), date.getMonth() , 1));
+		lastDay = formatDate(new Date(date.getFullYear(), date.getMonth() + 1, 0));
+		
+		assertEntriesData(firstDay,lastDay, entryData);
+
+	});
+
+	var assertEntriesData = function(firstDay, lastDay, entryData) {
+		expect(entryData).toEqual([
+			{date:firstDay,contact:"contact1",project:"project1",duration : "8"},
+			{date:firstDay,contact:"contact1",project:"project1",duration : "8"},
+			{date:lastDay,contact:"contact2",project:"project2",duration : "8"},
+		]);
+	};
+
+	var createEntriesData = function(entries) {
+		return entries.map(function(entry) {
 			return { 
 				date : entry.$(".date").getText(),
 				contact : entry.$(".contact").getText(),
@@ -17,17 +65,7 @@ describe('app', function() {
 				duration : entry.$(".duration").getText()
 			};
 		});
-		
-		var date = new Date();
-		var firstDay = formatDate(new Date(date.getFullYear(), date.getMonth(), 1));
-		var lastDay = formatDate(new Date(date.getFullYear(), date.getMonth() + 1, 0));
-		
-		expect(entryData).toEqual([
-			{date:firstDay,contact:"contact1",project:"project1",duration : "8"},
-			{date:firstDay,contact:"contact1",project:"project1",duration : "8"},
-			{date:lastDay,contact:"contact2",project:"project2",duration : "8"},
-		]);
-	});
+	};
 
   	var formatDate = function(date) {
         var actualMonth = date.getMonth() + 1;
