4d8983d4e8e79488182ab0c3f3dde1bdc234af24
Anshul Bajpai <bajpai.anshul@gmail.com>
2014-06-28 13:11:57 +0530

    Got rid of mongosync

--- test/protractor/protractorOnPrepare.js
+++ test/protractor/protractorOnPrepare.js
@@ -1,7 +1,5 @@
 var ScreenShotReporter = require('protractor-screenshot-reporter');
-var MongoServer = require("mongo-sync").Server
-var Fiber = require('fibers');
-
+var MongoClient = require('mongodb').MongoClient;
 require('jasmine-before-all');
 
 driver = browser.driver;
@@ -47,50 +45,48 @@ jasmine.getEnv().addReporter(new ScreenShotReporter({
     takeScreenShotsOnlyForFailedSpecs: true
 }));
 
-var authToken = {
-    "authToken" : "dd7e10bb-7df3-4f2f-b2c5-8ed1da2d255c",
-    "identifier" : "e190f5ff2fcc72ac2fa7be0ca36e10f9cf9c137f63a501b15fd346304d2f5001",
-    "date" : new Date()
-};
 
-var user = {
-    "identifier" : "e190f5ff2fcc72ac2fa7be0ca36e10f9cf9c137f63a501b15fd346304d2f5001",
-    "apiKey" : "WHS5Th2Lf/TxdTKItsEhsg==",
-    "accountId" : "valid_account_id",
-    "recordSalt" : "e25443ea-4a6d-452f-a3a7-41fa00b55e36",
-    "date" : new Date()
-};
+MongoClient.connect("mongodb://localhost:27017/minutedocktest",function(err,db) {
+    if(err) throw err;
+    var authToken = {
+        "authToken" : "dd7e10bb-7df3-4f2f-b2c5-8ed1da2d255c",
+        "identifier" : "e190f5ff2fcc72ac2fa7be0ca36e10f9cf9c137f63a501b15fd346304d2f5001",
+        "date" : new Date()
+    };
 
-var mongodb;
+    var user = {
+        "identifier" : "e190f5ff2fcc72ac2fa7be0ca36e10f9cf9c137f63a501b15fd346304d2f5001",
+        "apiKey" : "WHS5Th2Lf/TxdTKItsEhsg==",
+        "accountId" : "valid_account_id",
+        "recordSalt" : "e25443ea-4a6d-452f-a3a7-41fa00b55e36",
+        "date" : new Date()
+    };
+    var clearCollection = function(collectionName) {
+        var collection = db.collection(collectionName);    
+        collection.remove({},function(err) {
+            if(err) throw err;  
+        });
+    };
+    var addMongoDocument = function(collectionName, newDocument) {
+        var collection = db.collection(collectionName);    
+        collection.insert(newDocument, function(err, records) {
+            if(err) throw err;  
+        });
+    };    
 
-var clearCollection = function(collectionName) {
-    mongodb.getCollection(collectionName).drop();                
-};
+    clearUsers = function() {
+        clearCollection("users");        
+    };
 
-var addMongoDocument = function(collectionName, document) {
-    mongodb.getCollection(collectionName).insert(document);    
-}; 
+    persistAuthToken = function() {
+        addMongoDocument("authtokens",authToken);
+    };
 
-Fiber(function() {
-    mongodb = new MongoServer("localhost").db("minutedocktest");    
     clearCollection("authtokens");     
-    clearCollection("users");
+    clearCollection("users");  
     addMongoDocument("authtokens",authToken);
-    addMongoDocument("users",user);            
-}).run();
-
-clearUsers = function() {
-    Fiber(function() {
-        clearCollection("users"); 
-    }).run();
-};
-
-persistAuthToken = function() {
-    Fiber(function() {
-        addMongoDocument("authtokens",authToken);        
-    }).run();
-};
-
+    addMongoDocument("users",user);   
+});
 
 driver.get("/");
 resetSessionCookie();
