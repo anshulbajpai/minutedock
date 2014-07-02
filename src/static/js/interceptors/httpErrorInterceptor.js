6b290d98821fcf320c677bf9512129d6e169ebe2
Anshul Bajpai <bajpai.anshul@gmail.com>
2014-07-02 23:33:06 +0530

    Refactored  to $window instead of window

--- src/static/js/interceptors/httpErrorInterceptor.js
+++ src/static/js/interceptors/httpErrorInterceptor.js
@@ -1,6 +1,6 @@
 define(['modules/app'], function(app){	
   app.config(['$httpProvider',function($httpProvider) {
-    $httpProvider.interceptors.push(['$q','$location','$sessionStorage',function($q, $location, $sessionStorage) {        
+    $httpProvider.interceptors.push(['$q','$location','$sessionStorage','$window',function($q, $location, $sessionStorage, $window) {        
         return {
             'responseError': function(rejection) {
                 if(rejection.status == 403){
@@ -9,7 +9,7 @@ define(['modules/app'], function(app){
                     $location.path("/register");
                 }
                 else if(rejection.status == 401){
-                    window.location = "/"
+                    $window.location = "/"
                 }
                 return $q.reject(rejection);
             }
