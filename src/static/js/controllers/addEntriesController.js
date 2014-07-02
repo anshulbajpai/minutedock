0ef37d468e76df45015048695e8521bedeb7aae0
Anshul Bajpai <bajpai.anshul@gmail.com>
2014-07-02 23:31:45 +0530

    Fixed #67. Scroll to view entries after add

--- src/static/js/controllers/addEntriesController.js
+++ src/static/js/controllers/addEntriesController.js
@@ -1,10 +1,10 @@
 define(['modules/app','models/addEntriesModel','service/entriesService'] , function (app,AddEntriesModel) {
 
-  app.controller('addEntriesController',['$scope','$routeParams','$sessionStorage','$rootScope','entriesService', function($scope, $routeParams, $sessionStorage,$rootScope,entriesService){  	
+  app.controller('addEntriesController',['$scope','$location', '$anchorScroll','$routeParams','$sessionStorage','$rootScope','entriesService', function($scope, $location, $anchorScroll, $routeParams, $sessionStorage,$rootScope,entriesService){  	
 
     var model = new AddEntriesModel($sessionStorage.contacts, $sessionStorage.projects,$routeParams.month,$routeParams.year);
     $scope.model = model; 
-    
+
     this.selectProject = function(selectedProjectName) {
       model.selectProject(selectedProjectName);
     };
@@ -22,8 +22,10 @@ define(['modules/app','models/addEntriesModel','service/entriesService'] , funct
       .then(function() {
         model.selectedDates = [];
         model.allWeekdaysSelected = false;
-        $rootScope.alerts.success = "Entries added successfully!"     
-        $scope.$emit('parent.refresh.entries');
+        $rootScope.alerts.success = "Entries added successfully!";
+        $location.hash('viewEntries');
+        $anchorScroll();
+        $location.hash('');
       });
     };
 
