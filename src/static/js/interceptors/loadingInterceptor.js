e87531ba6aeeeb67e118b8d5f32f2430372074f3
Anshul Bajpai <bajpai.anshul@gmail.com>
2014-07-04 13:32:15 +0530

    Fixed bug where on page refresh, or prev/next - wait indicator disappears early

--- src/static/js/interceptors/loadingInterceptor.js
+++ src/static/js/interceptors/loadingInterceptor.js
@@ -2,23 +2,41 @@ define(['modules/app'], function(app){
   app.config(['$httpProvider',function($httpProvider) {
     $httpProvider.interceptors.push(['$q','$injector',function($q, $injector) {        
         var $rootScope;
+        var pendingRequests = 0;
+        
+        $rootScope = $rootScope || $injector.get('$rootScope');
+        
+        var requestReceived = function() {
+            ++ pendingRequests;
+            if(pendingRequests === 1){
+                $rootScope.ajaxLoader = true;                    
+            }            
+        };
+
+        var responseReceived = function() {
+            -- pendingRequests;
+            if(pendingRequests === 0){
+                $rootScope.ajaxLoader = false;                    
+            }            
+        };
+
         return {
            'request': function(config) {
-                $rootScope = $rootScope || $injector.get('$rootScope');
-                $rootScope.ajaxLoader = true;
+                requestReceived();
                 return config || $q.when(config);
             },
             'response': function(response) {
-                $rootScope = $rootScope || $injector.get('$rootScope');
-                $rootScope.ajaxLoader = false;
+                responseReceived();
                 return response || $q.when(response);
             },
             'responseError': function(rejection) {
-                $rootScope = $rootScope || $injector.get('$rootScope');
-                $rootScope.ajaxLoader = false;
+                responseReceived();
                 return $q.reject(rejection);
             }
         };
+
     }]);
+
   }]);
+
 });
